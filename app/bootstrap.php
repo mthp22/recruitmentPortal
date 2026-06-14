0fae2e0ddd36b5acebe0439a2d5d73873d6f83b3
mthp22 <221026639@students.uj.ac.za>
2026-06-14 18:59:12 +0200

    (bootstrap) add autoload bootstrap

---
+++ app/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+$vendorAutoload = dirname(__DIR__) . '/vendor/autoload.php';
+if (is_file($vendorAutoload)) {
+    require_once $vendorAutoload;
+
+    return;
+}
+
+spl_autoload_register(static function (string $class): void {
+    $prefix = 'App\\';
+    if (!str_starts_with($class, $prefix)) {
+        return;
+    }
+
+    $relative = substr($class, strlen($prefix));
+    $path = __DIR__ . '/' . str_replace('\\', '/', $relative) . '.php';
+
+    if (is_file($path)) {
+        require_once $path;
+    }
+});
